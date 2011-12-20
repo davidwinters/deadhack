933e600ac37994f87b9b0ce042086715fa1a8a87
David Winters <mac@mpcstudios.com>
2011-12-20 16:53:36 -0600

    now with dungeon generation

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
@@ -53,21 +56,106 @@ class Thing(object):
 	def clear(self):
 		#erase the ghosts when moving around
 		libtcod.console_put_char_ex(con, self.x, self.y, '.', libtcod.light_grey, libtcod.BKGND_NONE)
-		
+
+
+class Rect(object):
+	#this is for dungeon building
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h	
+	
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+	
+	def intersect(self, other):
+		#tell us if this rect intersects another
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def make_room(room):
+	global map
+	#make tiles in rectangle passable
+	for x in range(room.x1 + 1 , room.x2): 
+		for y in range(room.y1 + 1, room.y2):
+			map[x][y].blocked = False
+			map[x][y].block_sight = False	
+
+def make_h_tunnel(x1, x2, y):
+	global map
+	#make horizontal tunnel
+	for x in range(min(x1, x2), max(x1, x2) +1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
+def make_v_tunnel(y1, y2, x):
+	global map
+	#make vertical tunnel
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
 			
 def make_map():
 	global map
 
 	#fill map with unblocked tiles
-	map = [[ Tile(False)
-		for y in range(MAP_HEIGHT)]
-			for x in range(MAP_WIDTH)]
+	map = [[ Tile(True)
+		for y in range(MAP_HEIGHT) ]
+			for x in range(MAP_WIDTH) ]
 
-	map[30][22].blocked = True
-	map[30][22].block_sight = True
-	map[55][22].blocked = True
-	map[55][22].block_sight = True
+	#our map algorithm
+	rooms = []
+	num_rooms = 0
+	for r in range(MAX_ROOMS):
+		#random width and height
+		w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		#pick a random starting point
+		x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+		y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
 
+		#lets take advantage of our Rect class now
+		new_room = Rect(x, y, w, h)
+
+		#do our rooms intersect?
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+
+		#if not lets carve it
+		if not failed:
+			make_room(new_room)
+
+			#grabbing center coordinates for some reason
+			(new_x, new_y) = new_room.center()
+
+			if num_rooms == 0:
+				#this must be first room, start player here at center coordinates
+				player.x = new_x
+				player.y = new_y
+
+			else: #this is not the first room so lets make a tunnel now
+				(prev_x, prev_y) = rooms[num_rooms-1].center()
+
+				#draw a coin, if 1 go horizontal first, if 0 go vertical first
+				if libtcod.random_get_int(0, 0, 1) ==1:
+					#go horizontal first
+					make_h_tunnel(prev_x, new_x, prev_y)
+					make_v_tunnel(prev_y, new_y, new_x)
+
+				else:
+					#go vertical first
+					make_v_tunnel(prev_y, new_y, prev_x)
+					make_h_tunnel(prev_x, new_x, new_y)
+			
+			#put the room in our list
+			rooms.append(new_room)
+			num_rooms += 1
+	
 def render_all():
 
 	
