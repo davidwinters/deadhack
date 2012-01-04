d2299db9a624fa488caf28600fd912a2cc5c2016
David Winters <mac@mpcstudios.com>
2012-01-04 08:43:41 -0600

    oops

--- dickslayer.py
+++ dickslayer.py
@@ -1,10 +1,11 @@
 from lib import libtcodpy as libtcod
+from dickslayer import dungeon
 import math
 import textwrap
 import shelve
 
 #########################################
-#  1 DEFINE PARAMETERS                  #
+#  1 DEFINE CONSTANTS                   #
 #                                       #
 #########################################
 
@@ -52,15 +53,7 @@ color_light_ground = libtcod.Color(192, 192, 192) #light grey
 #                                       #
 #########################################
 
-class Tile(object):
-	#tiles on the map
-	def __init__(self, blocked, block_sight = None):
-		self.blocked = blocked
-		self.explored = False
 
-		#blcoked tiles also block sight
-		if block_sight is None: block_sight = blocked
-		self.block_sight = block_sight
 
 
 class Thing(object): #i renamed this to Thing instead of Object just cause
@@ -222,109 +215,8 @@ class Item(object):
 		self.owner.y = player.y
 		message('You dropped a ' + self.owner.name + '.', libtcod.yellow)
 					
+#map stuff removed from here
 
-
-class Rect(object):
-	#this is for dungeon building
-	def __init__(self, x, y, w, h):
-		self.x1 = x
-		self.y1 = y
-		self.x2 = x + w
-		self.y2 = y + h	
-	
-	def center(self):
-		center_x = (self.x1 + self.x2) / 2
-		center_y = (self.y1 + self.y2) / 2
-		return (center_x, center_y)
-	
-	def intersect(self, other):
-		#tell us if this rect intersects another
-		return (self.x1 <= other.x2 and self.x2 >= other.x1 and self.y1 <= other.y2 and self.y2 >= other.y1)
-
-def make_room(room):
-	global map
-	#make tiles in rectangle passable
-	for x in range(room.x1 + 1 , room.x2): 
-		for y in range(room.y1 + 1, room.y2):
-			map[x][y].blocked = False
-			map[x][y].block_sight = False	
-
-def make_h_tunnel(x1, x2, y):
-	global map
-	#make horizontal tunnel
-	for x in range(min(x1, x2), max(x1, x2) +1):
-		map[x][y].blocked = False
-		map[x][y].block_sight = False
-
-def make_v_tunnel(y1, y2, x):
-	global map
-	#make vertical tunnel
-	for y in range(min(y1, y2), max(y1, y2) + 1):
-		map[x][y].blocked = False
-		map[x][y].block_sight = False
-			
-def make_map():
-	global map, objects
-
-	objects = [player]
-
-	#fill map with blocked tiles because its True, if False all tiles would be unblocked
-	map = [[ Tile(True)
-		for y in range(MAP_HEIGHT) ]
-			for x in range(MAP_WIDTH) ]
-
-	#our map algorithm
-	rooms = []
-	num_rooms = 0
-	for r in range(MAX_ROOMS):
-		#random width and height
-		w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-		h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-		#pick a random starting point
-		x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
-		y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
-
-		#lets take advantage of our Rect class now
-		new_room = Rect(x, y, w, h)
-
-		#do our rooms intersect?
-		failed = False
-		for other_room in rooms:
-			if new_room.intersect(other_room):
-				failed = True
-				break
-
-		#if not lets carve it
-		if not failed:
-			make_room(new_room)
-			#add some contents to this room, such as monsters
-			place_things(new_room)
-
-			#grabbing center coordinates for some reason
-			(new_x, new_y) = new_room.center()
-
-			if num_rooms == 0:
-				#this must be first room, start player here at center coordinates
-				player.x = new_x
-				player.y = new_y
-
-			else: #this is not the first room so lets make a tunnel now
-				(prev_x, prev_y) = rooms[num_rooms-1].center()
-
-				#draw a coin, if 1 go horizontal first, if 0 go vertical first
-				if libtcod.random_get_int(0, 0, 1) == 1:
-					#go horizontal first
-					make_h_tunnel(prev_x, new_x, prev_y)
-					make_v_tunnel(prev_y, new_y, new_x)
-
-				else:
-					#go vertical first
-					make_v_tunnel(prev_y, new_y, prev_x)
-					make_h_tunnel(prev_x, new_x, new_y)
-			
-			#put the room in our list
-			rooms.append(new_room)
-			num_rooms += 1
 
 def place_things(room):
 	#random number of monsters
@@ -691,7 +583,7 @@ def main_menu():
 
 
 def new_game():
-	global player, inventory, game_msgs, game_state
+	global player, inventory, game_msgs, game_state, objects, map
 
 	fighter_comp = Fighter(hp=30, defense=2, power=5, death_function=player_death)
 	player = Thing(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', 'Joe', libtcod.white, blocks=True, fighter=fighter_comp)
@@ -704,7 +596,7 @@ def new_game():
 	inventory = []
 
 	#create the map
-	make_map()
+	map = dungeon.make_map(MAP_HEIGHT, MAP_WIDTH, MAX_ROOMS, ROOM_MIN_SIZE, ROOM_MAX_SIZE, player)
 
 	initialize_fov()
 	
