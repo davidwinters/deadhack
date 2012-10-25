6f4ea24e56a59ca3dc3f640a08e194113e0b8544
Burn <lol@you.com>
2012-10-25 11:45:00 -0500

    trying to push initial simple files

--- deadhack.py
+++ deadhack.py
@@ -1,1 +1,19 @@
-#this game sucks
+#
+# deadhack
+#
+
+
+#main lib import - has all key functions
+#is the main lib we are working with
+from lib import libtcodpy as libtcod
+
+from lib import display
+#libtcod.console_set_custom_font('terminal10x10_gs_tc.png',
+#                                         libtcod.FONT_TYPE_GREYSCALE |
+#                                         libtcod.FONT_LAYOUT_TCOD)
+#screen_width = 80
+#screen_height = 80
+#libtcod.console_init_root(screen_width, screen_height,
+#                                        'deadhack', False)
+display.init()
+print 'done!'
