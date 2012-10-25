7acb6e85814cf1e62521f11e0a6ce8617a2f6558
Burn <lol@you.com>
2012-10-25 17:28:13 -0500

    some initial structure, pretty painfully slow so minimal so far.

--- deadhack.py
+++ deadhack.py
@@ -1,16 +1,57 @@
 #
 # deadhack
 #
-
-
-#display library for drawing everything on screen
+#http://roguebasin.roguelikedevelopment.org/index.php?title=Complete_Roguelike_Tutorial,_using_python%2Blibtcod,_part_1
+#
+# imports
+#
+#display library for trying to encapsulate graphics
 from lib import display
-
-
+#import libtcod - just required, has many parts
+#like keyboard processing outside of graphics
+from lib import libtcodpy as libtcod
+#made a placeholder but not used yet
+#from lib import Player
+#I needed support methods for logic and didn't know where to put them
+from lib import support
 #
+# main method
 #
-#
-
-
+#init basic display items
 display.init()
-print 'done!'
+#using mode as a placeholder here for game state,
+#since we by default start mid-game.
+mode = 'map'
+
+
+#
+# main logic loop
+#
+while not display.console():
+    #draw() --what's this?
+
+    #flush state to viewport this cycle
+    display.flush()
+    
+    #get user input for game loop
+    key = libtcod.console_wait_for_keypress(True)
+    #process key we're given
+    support.process_key(key,mode)
+    #once we get the key things seem complicated.
+    #some keys are player actions,
+    #others are meta-game commands like option or quit
+    #others are simply to get status info like inventory
+    #that later involve game state updates =|
+
+    #perform game logic
+    #there may be many steps here!
+    #things like timers, status effects, player updates, watever
+    #update(key)
+
+
+    #break out of game - should this be moved elsewhere?
+    if key.vk == libtcod.KEY_ESCAPE:
+        break
+
+print 'game finished!'
+
--- lib/display.py
+++ lib/display.py
@@ -18,5 +18,20 @@ def init():
     screen_height = 80
     libtcod.console_init_root(screen_width, screen_height, 'deadhack', False)
     libtcod.console_set_foreground_color(0, libtcod.white)
+    #print an intiial actor?
     libtcod.console_print_left(0, 1, 1, libtcod.BKGND_NONE, '@')
+    #flush screen to viewport
+    libtcod.console_flush()
+
+#
+# logic for knowing main window is still open
+# used in main game loop
+#
+def console():
+    return libtcod.console_is_window_closed()
+
+#
+# push visual changes to screen 'viewport'
+#
+def flush():
     libtcod.console_flush()
