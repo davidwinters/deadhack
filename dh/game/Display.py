3387c996e4f04c666dddba705675237e05d245c1
David Winters <mac@mpcstudios.com>
2012-10-29 23:19:11 -0500

    cleaning mess

--- dh/game/Display.py
+++ dh/game/Display.py
@@ -1,10 +1,8 @@
 from ..lib import libtcodpy as libtcod
 #Player import may be removed in future for this class
-import Player
+
 
 class Display():
-
-    con = []
 
     def __init__(self):
         '''initialize game console'''
@@ -13,36 +11,32 @@ class Display():
                                         libtcod.FONT_TYPE_GREYSCALE |
                                         libtcod.FONT_LAYOUT_TCOD)
         #init window
-        screen_width = 80
-        screen_height = 80
-        libtcod.console_init_root(screen_width, screen_height, 'deadhack', False)
+        self.screen_width = 80
+        self.screen_height = 80
+        libtcod.console_init_root(self.screen_width, self.screen_height, 'deadhack', False)
         libtcod.console_set_foreground_color(0, libtcod.white)
         #flush screen to viewport
         libtcod.console_flush()
         #setup offscreen console
-        self.con = libtcod.console_new(screen_width,screen_height)
-
-
+        self.con = libtcod.console_new(self.screen_width, self.screen_height)
 
     def draw(self, mode, player):
         """ draw the screen pre-blit step 1 tut"""
         #if player has moved from last position
-        libtcod.console_print_left(con, player.x, player.y, libtcod.BKGND_NONE, '@')
+        libtcod.console_print_left(self.con, player.x, player.y, libtcod.BKGND_NONE, '@')
         #save this position for next iteration
-        libtcod.console_blit(con,0,0,screen_width,screen_height,0,0,0)
+        libtcod.console_blit(self.con, 0, 0, self.screen_width, self.screen_height, 0, 0, 0)
         libtcod.console_flush()
-
 
     def clear(self, mode, player):
         '''ghetto clear used in step 1 of tut pre-blit'''
-        libtcod.console_print_left(con, player.x, player.y, libtcod.BKGND_NONE, ' ')
-        libtcod.console_blit(con,0,0,screen_width,screen_height,0,0,0)
+        libtcod.console_print_left(self.con, player.x, player.y, libtcod.BKGND_NONE, ' ')
+        libtcod.console_blit(self.con, 0, 0, self.screen_width, self.screen_height, 0, 0, 0)
         libtcod.console_flush()
 
     # logic for knowing main window is still open
     # used in main game loop
     #
-
 
     def display_closed(self):
         return libtcod.console_is_window_closed()
